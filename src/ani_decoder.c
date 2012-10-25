6b5d36760ac37f0f97a546702802c952bb5d2e35
Ilya Arefiev <arefiev.id@gmail.com>
2012-10-25 11:24:09 +0400

    determine ANI request end correctly

--- src/ani_decoder.c
+++ src/ani_decoder.c
@@ -163,13 +163,14 @@ EXIT_STATUS bb_ani_decode( const char* _infname )
     ANI_DECODER_STATE dstate = ADS_WAIT_FOR_SIGNAL;
     struct PEAK_STRUCT {
         float value;
-        float tm;
-    };
-    struct PEAK_STRUCT peaks[3] = { {0} };
+        size_t tm;
+    } peaks[2] = { {0} };
     int peakn = 0;
     const int peaksToMiss = 2;
     const float peaktrs = 0.7;
-    size_t peakPeriod = 0;
+    size_t peakSyncDur = (size_t)S2SAMPLES( ANI_REQUEST_DUR/1000.0, opt.samplerate );
+    size_t peakPeriod = (size_t)S2SAMPLES( (1/500.0), opt.samplerate ); /*initial period value to tune*/
+    size_t firstPeakSample = 0;
     while ( (readcount = sf_readf_float( ifd, block, blockcount )) > 0 )
     {
         for ( samplen = 0; samplen < readcount; samplen++ )
@@ -250,74 +251,106 @@ EXIT_STATUS bb_ani_decode( const char* _infname )
                                         , tm
                                       );
 #endif
-                               /*for ( i = 0; i < ANI_FREQ_COUNT; i++ )*/
-                               /*{*/
-                               /*    GOERTZEL_DATA* data = &(gdata[ i*opt.channels + ch ]);*/
-                               /*    bbg_save_start_stop( data, true );*/
-                               /*}*/
+                                peakSyncDur += tm_sample;
 
-                               dstate = ADS_WAIT_FOR_PEAK;
-                               wfd[ch].tm = tm;
+                                dstate = ADS_WAIT_FOR_PEAK;
+                                wfd[ch].tm = tm;
                             }
                             break;
                         }
                     case ADS_WAIT_FOR_PEAK:
                         {
-                            /*avoid neighbour dots in the same peak*/
-                            if ( update_wf )
+                            if ( samplev > peaktrs )
                             {
-                                if ( samplev > peaktrs )
+                                if (    peakn
+                                     && (tm_sample - peaks[0].tm) < peakPeriod/2
+                                   )
                                 {
-                                    if ( samplev > peaks[ peakn ].value )
+#ifdef _DEBUG
+                                    printf( "avoid neighbour points in the same peak: %zu < %zu\n"
+                                            , ( tm_sample - peaks[0].tm )
+                                            , ( peakPeriod/2 )
+                                            );
+#endif
+                                }
+                                else
+                                {
+                                    if ( samplev > peaks[peakn].value )
+                                    {
+                                        peaks[peakn].value = samplev;
+                                        peaks[peakn].tm = tm_sample;
+                                    }
+                                    else
                                     {
 #ifdef _DEBUG
-                                        printf( "peak #%d @ %0.6f: %0.6f\n"
-                                                , peakn
+                                        printf( "found peak @ %0.6f (%zu sample)\n"
                                                 , tm
-                                                , samplev
+                                                , tm_sample
                                               );
 #endif
-
-                                        peaks[ peakn ].value = samplev;
-                                        peaks[ peakn ].tm = tm;
-
-                                        peakn++;
-
-                                        if ( peakn == 3 )
-                                        {
-                                            dstate = ADS_WAIT_FOR_SYNC;
-                                        }
+                                        dstate = ADS_PEAK_SYNC;
                                     }
                                 }
                             }
                             break;
                         }
-                    case ADS_WAIT_FOR_SYNC:
+                    case ADS_PEAK_SYNC:
                         {
-                            float T = AVG(   ABS(peaks[1].tm - peaks[0].tm)
-                                           , ABS(peaks[2].tm - peaks[1].tm)
-                                         );
-                            peakPeriod = (size_t)S2SAMPLES( T, opt.samplerate );
+                            if ( tm_sample < peakSyncDur )
+                            {
+                                if ( peakn )
+                                {
 #ifdef _DEBUG
-                            printf( "peak period %0.6f\n"
-                                    , T
-                                  );
+                                    size_t pp = peakPeriod;
+
+                                    printf( "%zu samples betw peaks\n"
+                                            , (peaks[1].tm - peaks[0].tm)
+                                          );
 #endif
+                                    peakPeriod = AVG(   peakPeriod
+                                                      , ABS( peaks[1].tm - peaks[0].tm )
+                                                    );
 
-                            peakn = peaksToMiss;
-                            dstate = ADS_WAIT_FOR_REQUEST_END;
+                                    memcpy( peaks, peaks+1, sizeof( struct PEAK_STRUCT ) );
+                                    memset( peaks+1, 0, sizeof( struct PEAK_STRUCT ) );
 
+#ifdef _DEBUG
+                                    printf( "peakPeriod: %zu -> %zu samples\n"
+                                            , pp
+                                            , peakPeriod
+                                            );
+#endif
+                                }
+                                else
+                                {
+#ifdef _DEBUG
+                                    printf( "wait for next peak\n" );
+#endif
+                                    firstPeakSample = peaks[0].tm;
+                                    peakn++;
+                                }
+
+                                dstate = ADS_WAIT_FOR_PEAK;
+                            }
+                            else
+                            {
+                                peakn = peaksToMiss;
+                                dstate = ADS_WAIT_FOR_REQUEST_END;
+                            }
                             break;
                         }
                     case ADS_WAIT_FOR_REQUEST_END:
                         {
-                            if ( tm_sample % peakPeriod == 0 )
+                            if ( (tm_sample - firstPeakSample) % peakPeriod == 0 )
                             {
                                 if ( samplev < peaktrs )
                                 {
 #ifdef _DEBUG
-                                    printf( "missing peak @ %0.6f\n"
+                                    printf( "missing peak @ %0.6f (%zu sample): %0.6f < %0.6f\n"
                                             , tm
+                                            , tm_sample
+                                            , samplev
+                                            , peaktrs
                                           );
 #endif
                                     peakn--;
@@ -329,11 +362,20 @@ EXIT_STATUS bb_ani_decode( const char* _infname )
                                                 , tm
                                               );
 #endif
+                                        for ( i = 0; i < ANI_FREQ_COUNT; i++ )
+                                        {
+                                            GOERTZEL_DATA* data = &(gdata[ i*opt.channels + ch ]);
+                                            bbg_goertzel_reset( data );
+                                        }
+
                                         dstate = ADS_WAIT_FOR_ANI_END;
                                     }
                                 }
                                 else
                                 {
+#ifdef _DEBUG
+                                    printf( "restore missing peaks\n" );
+#endif
                                     peakn = peaksToMiss;
                                 }
                             }
